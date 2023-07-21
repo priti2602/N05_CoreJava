348a28b6d54cf2149415b33ace0c2e5400e26716
priti2602 <shindepriti2602@gmail.com>
2023-07-20 23:01:34 -0700

    Second Commit

---
+++ dayone/src/org/tnsif/operators/BitwiseOperatorDemo.java
@@ -0,0 +1,18 @@
+//Program to demonstrate on Bitwise operator
+
+package org.tnsif.operators;
+
+public class BitwiseOperatorDemo {
+
+	public static void main(String[] args) {
+		int x=15,y=3;
+		System.out.println(x&y);
+		System.out.println(x|y);
+		System.out.println(x^y);
+		System.out.println(x>>y);
+		System.out.println(x<<y);
+		
+
+	}
+
+}
---
+++ dayone/src/org/tnsif/operators/IncrementOperatorDemo.java
@@ -0,0 +1,17 @@
+//Program to demonstrate on Pre and Post Increment
+package org.tnsif.operators;
+
+public class IncrementOperatorDemo {
+
+	public static void main(String[] args) {
+		int p=11,q=2;
+		int x=p++;
+		int y=p+q;
+		++q;
+		System.out.println(p);
+		System.out.println(q);
+		
+
+	}
+
+}
