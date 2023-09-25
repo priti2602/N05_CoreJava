de1dec35d8b8cd134cb09ee4bccfb2b429a1db6f
priti2602 <shindepriti2602@gmail.com>
2023-09-25 22:39:38 +0530

    First commit

---
+++ daythirteen/src/org/tnsif/daythirteen/ThreadDemo.java
@@ -0,0 +1,14 @@
+package org.tnsif.daythirteen;
+
+public class ThreadDemo {
+
+	public static void main(String[] args) {
+		
+		Thread t = Thread.currentThread();
+		System.out.println(t.getName());
+		System.out.println(t);
+		
+		System.out.println(new ThreadDemo());
+	}
+
+}
